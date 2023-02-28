3eff120bdcb72366a8281fbc647f695114bc8498
Niz97 <97niran@gmail.com>
2023-02-28 22:03:20 +0000

    Initial setup

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import { config } from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+config()
+
+const app = express();
+
+app.use(cors({
+  origin: "*",
+}));
+app.use(express.json());
+
+const connect_string = process.env.MONGO_URI;
+const PORT = 5000;
+
+mongoose.connect(
+  connect_string!
+  ).then(() => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
+app.listen(5000);
