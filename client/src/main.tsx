f6034b6fb8e91e3c009a15ba869b93e884a337eb
Niz97 <97niran@gmail.com>
2023-03-02 21:27:03 +0000

    Removed all remaining tracker project code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import Tracker from './Tracker';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: '/trackers/:trackerId',
-    element: <Tracker/>
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
