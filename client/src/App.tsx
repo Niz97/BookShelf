e1aab184ee2c415dd3fc00304cb7311ac3d16cf6
Niz97 <97niran@gmail.com>
2023-03-04 17:57:25 +0000

    Minor styling for book, created <Book /> component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import './App.css'
+import Book from './components/Book'
 
 function App() {
   return (
     <div className="App">
       <div className="shelf-container">
-        <div>
-          <h3>Stormbreaker</h3>
-          <h4>Sci-fi</h4>
-          <h5>Some author</h5>
-        </div>
+        <Book />
       </div>
     </div>
   )
---
+++ client/src/components/Book.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Book() {
+  return (
+    <div className="book">
+      <p>Stormbreaker</p>
+      <p>Sci-fi</p>
+      <p>Some author</p>
+    </div>
+  )
+}
+
+export default Book
