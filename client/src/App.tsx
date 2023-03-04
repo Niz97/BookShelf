5c04d077d0abb26f17f2c11ddc1b2beb7f682c16
Niz97 <97niran@gmail.com>
2023-03-04 23:38:31 +0000

    Basic book form and minor styling to form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Book from './components/Book'
 
+
 function App() {
+  const [book, setBook] = useState({
+    name: '',
+    genre: '',
+    author: ''
+  });
+  
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
+    console.log('submitting...');
+  }
+
+  const handleChange = (e:React.FormEvent) => {
+    setBook((prevState) => ({
+      ...prevState,
+      [(e.target as HTMLTextAreaElement).name]: (e.target as HTMLTextAreaElement).value
+    }))
+  }
   return (
     <div className="App">
       <div className="shelf-container">
         <Book />
+        <Book />
+        <Book />
+      </div>
+      <div className="review-form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input 
+              id="book-name" 
+              name="name"
+              type="text"
+              value={book.name}
+              onChange={handleChange}
+            />
+            <label htmlFor="genre">Genre</label>
+            <input 
+              id="book-genre" 
+              name="genre"
+              type="text"
+              value={book.genre}
+              onChange={handleChange}
+            />
+            <label htmlFor="name">Author</label>
+            <input 
+              id="book-author" 
+              name="author"
+              type="text"
+              value={book.author}
+              onChange={handleChange}
+            />
+            <button type="submit">
+              Add Book
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
